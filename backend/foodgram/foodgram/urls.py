8a31d5461e7b0f1aedbb0bdbdb7c5bd16b3cc2c1
Владислав Бородай <vrboroday@yandex.ru>
2023-03-23 21:28:15 +0300

    urls djoser

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -13,8 +13,8 @@ router.register(r'recipes', RecipesViewSet)
 router.register(r'ingredients', IngredientViewSet)
 
 urlpatterns = [
-    path('api/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
+    path('api/', include('djoser.urls')),
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
