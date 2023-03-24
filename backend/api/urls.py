90872062a1fe1f472ae3292df33bbd4d4a4ceb5e
Владислав Бородай <vrboroday@yandex.ru>
2023-03-24 21:01:24 +0300

    final

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -11,6 +11,6 @@ router.register(r"tags", TagViewSet, basename="tags")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
+    path("", include("djoser.urls")),
 ]
